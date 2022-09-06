97336ae48b646c415f24d2cf2f064e8ba0fe2370
Yair G <yjgarizabalo@gmail.com>
2022-09-06 18:49:49 -0500

    [pokemon] search

--- components/views/DetallePokemon.js
+++ components/views/DetallePokemon.js
@@ -1,13 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
 
 
 const DetallePokemon = ({ navigation, route }) => {
   const [info, setInfo] = useState([]);
   const [nombrePok, setNombrePok] = useState();
+  const [exp, setExp] = useState([]);
   const [imgPok, setImgPok] = useState([]);
-
+  const [peso, setPeso] = useState();
+  const [altura, setAltura] = useState();
 
   useEffect(() => {
     fetchData();
@@ -21,8 +23,11 @@ const DetallePokemon = ({ navigation, route }) => {
       const response = await fetch(route.params.url_pok, options);
       const json = await response.json();
       setInfo(json.results)
-      setNombrePok(json.name.toUpperCase());
+      setNombrePok(json.name);
+      setExp(json.base_experience);
       setImgPok(json.sprites);
+      setPeso(json.weight)
+      setAltura(json.height)
       console.log(json);
     } catch (error) {
       console.log("error", error);
@@ -30,9 +35,31 @@ const DetallePokemon = ({ navigation, route }) => {
   }
   return (
     <View style={styles.container}>
-      <Image style={styles.image} source={{uri: imgPok.front_default}} />
-      <Text style={styles.name}>{nombrePok}</Text>
-      <StatusBar style="auto" />
+      <View style={{ backgroundColor: '#f2f2f2', paddingLeft: 20, paddingBottom: 20 }}>
+      <View style={{ alignItems: 'center'}}>        
+        <Image style={styles.image} source={{ uri: imgPok.front_default }} />
+      </View>
+
+        <View style={{paddingTop: 20}}>
+          <Text style={styles.title}>Nombre</Text>
+          <Text style={styles.name}>{nombrePok}</Text>
+        </View>
+
+        <View style={{paddingTop: 20}}>
+          <Text style={styles.title}>Experience</Text>
+          <Text style={styles.name}>{exp}</Text>
+        </View>
+        <View style={{paddingTop: 20}}>
+          <Text style={styles.title}>Peso</Text>
+          <Text style={styles.name}>{peso} lb</Text>
+        </View>
+
+        <View style={{paddingTop: 20}}>
+          <Text style={styles.title}>Altura</Text>
+          <Text style={styles.name}>{altura} ft</Text>
+        </View>
+        <StatusBar style="auto" />
+      </View>
     </View>
   );
 }
@@ -40,17 +67,23 @@ const DetallePokemon = ({ navigation, route }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
     justifyContent: 'center',
+    backgroundColor: '#ffffff',
+    padding: 40,
+  },
+  title: {
+    fontSize: 12,
+    fontWeight: 'bold',
+    color: '#777777'
   },
   name: {
     fontSize: 17,
     color: '#1c1c1c',
+    textAlign: 'left',
   },
   image: {
-    width: 200,
-    height: 200,
+    width: 170,
+    height: 170,
     resizeMode: "contain",
   },
 });
--- components/views/SearchPokemon.js
+++ components/views/SearchPokemon.js
@@ -1,12 +1,19 @@
+import React, { useState, useEffect } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View,TextInput, SafeAreaView, FlatList } from 'react-native';
 
 
-export default function SearchPokemon() {
+const SearchPokemon =() => {
   return (
     <View style={styles.container}>
-      <Text>Buscar Pokémones</Text>
       <StatusBar style="auto" />
+      <TextInput
+          style={styles.textInputStyle}
+          onChangeText={(text) => searchFilterFunction(text)}
+          // value={search}
+          // underlineColorAndroid="transparent"
+          // placeholder="Search Here"
+        />
     </View>
   );
 }
@@ -18,4 +25,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  textInputStyle: {
+    height: 40,
+    borderWidth: 1,
+    paddingLeft: 20,
+    margin: 5,
+    borderColor: '#323232',
+    backgroundColor: '#FFFFFF',
+  },
 });
+
+export default SearchPokemon
